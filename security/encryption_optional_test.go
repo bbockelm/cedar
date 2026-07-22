dd95afb48b2b277c13c4d8dc495c8e5085158238
Brian Bockelman <bbockelman@morgridge.org>
2026-07-21 19:16:16 -0500

    test: check ln.Close error (errcheck lint)

--- security/encryption_optional_test.go
+++ security/encryption_optional_test.go
@@ -23,7 +23,7 @@ func TestOptionalEncryptionRecordedWhenActive(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer ln.Close()
+	defer func() { _ = ln.Close() }()
 
 	cfg := func() *SecurityConfig {
 		return &SecurityConfig{
